fd8366531faaac1af19a49cd4a82afae263f80e1
josepplloo <josepplloo@gmail.com>
2017-10-23 21:23:54 -0500

    todos los capitulos

--- informe/src/underbrush.py
+++ informe/src/underbrush.py
@@ -5,7 +5,9 @@ df = pd.read_csv(infile, header = 0)
 for aux in df.DxPrincipal.unique():
     print aux
     bee = df[df['DxPrincipal']==aux]
-    bee.to_csv("/Users/josepplloo/Documents/scripts/RIPS_2013_1/2splitDx/%s.csv" %aux,index=False)
+    bee.to_csv("/Users/josepplloo/Documents/
+    	scripts/RIPS_2013_1/2splitDx/%s.csv"
+    	 %aux,index=False)
 
 
 
